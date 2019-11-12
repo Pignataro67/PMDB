f17be3f80c3c294addf307048e8331afbe243a6c
Pignataro67 <Pignataro67@gmail.com>
2019-11-12 10:54:46 -0800

    removing logo, personalizing App, adding cont.

---
+++ client/src/containers/MoviesContainers.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AddMovie from '../components/AddMovies';
+
+
+class MoviesContainer extends Component {
+  render(){
+    return(
+      <div>
+        <Movies />
+      </div>
+    )
+  }
+}
+
+export default MoviesContainer;
