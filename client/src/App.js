5b4ce04d7ec20eea4798d58ea43cf549050c20af
Pignataro67 <Pignataro67@gmail.com>
2020-05-28 10:26:54 -0700

    adding controller

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home';
-import MoviesContainer from './containers/MoviesContainer';
+// import React from 'react';
+// // import logo from './logo.svg';
+// import './App.css';
+// import Home from './components/Home';
+// import MoviesContainer from './containers/MoviesContainer';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Movies</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//     <h1>Movies</h1>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- client/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const Home = () => (
-  <div className="home-page">
-    Welcome Home
-  </div>
-)
-
-export default Home;
